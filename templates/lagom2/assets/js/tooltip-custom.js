3d29533bb52f76faff74c3e448c98dd6123fd9f4
Sofiya <sofik_shu2@mail.ru>
2024-04-10 23:58:53 +0300

    added custom tooltip script file for map

---
+++ templates/lagom2/assets/js/tooltip-custom.js
@@ -0,0 +1,27 @@
+const title = document.querySelector('.title-country');
+const tooltip = document.querySelector('.tooltip');
+const tooltipImg = document.querySelector('.flag-country');
+const countries = document.querySelectorAll('.country');
+const bigArea1 = document.querySelector('.big-area-uae');
+const smArea1 = document.querySelector('.cntry-uae');
+const bigArea2 = document.querySelector('.big-area-singapore');
+const smArea2 = document.querySelector('.cntry-singapore');
+
+countries.forEach(country =>{
+    country.addEventListener('mousemove', function(e){
+      
+      title.innerText = this.dataset.title;
+      tooltip.style.top = (e.pageY - 3220) + 'px';
+      tooltip.style.left = (e.pageX - 40) + 'px';
+      tooltipImg.src=this.dataset.flag;
+
+    });
+
+    country.addEventListener('mouseenter', function(){
+        tooltip.style.display = "block";
+    });
+
+    country.addEventListener('mouseleave', function(){
+        tooltip.style.display = "none";
+    });
+})
