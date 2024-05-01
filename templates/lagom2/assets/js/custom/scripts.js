9a0abde5a023b6f9034a93e8866d8419a16a7cf1
Sofiya <sofik_shu2@mail.ru>
2024-05-01 23:58:11 +0300

    deleted useless scripts

--- templates/lagom2/assets/js/custom/scripts.js
+++ templates/lagom2/assets/js/custom/scripts.js
@@ -1,35 +1,3 @@
-//header-switcher: currency
-document.addEventListener('DOMContentLoaded', function() {
-    const currencyButtons = document.querySelectorAll('.switcher__item-currency');
-    const currentPage = '';
-  
-    currencyButtons.forEach(button => {
-        const key = button.getAttribute('data-key');
-        var buttonLink = '';
-  
-        switch(key){
-          case "usd":
-            buttonLink = '/index.php?currency=1';
-            break;
-          case "rub":
-            buttonLink = '/index.php?currency=2';
-            break;
-          case "eur":
-            buttonLink = '/index.php?currency=3';
-            break;
-        }
-        
-        button.addEventListener('click', function() {
-            window.location.href = buttonLink;
-        });
-        
-        if (key === currency) {
-            button.classList.add('active-button'); 
-        }
-    });
-});
-  
-
 // header scrolling
 $(function() {
     let header = $('.app-nav-menu');
@@ -57,33 +25,3 @@ for (var i = 0; i < 4; i++) {
 
 liElement.appendChild(buttonElement);
 
-
-// header adding switchers
-var liElementMenu = document.querySelector('li.sidebar-menu-switchers');
-var tplContent = `
-<div class="switcher-wrapper">
-    <div class="switcher sw-white switcher-sidebar">
-    <button class="switcher__item-currency switcher__item btn btn-switcher" data-key="usd">
-        USD
-    </button>
-    <button class="switcher__item-currency switcher__item btn btn-switcher" data-key="rub">
-        RUB
-    </button>
-    <button class="switcher__item-currency switcher__item btn btn-switcher" data-key="eur">
-        EUR
-    </button>
-    </div>
-</div>
-<div class="switcher-wrapper">
-    <div class="switcher sw-white switcher-sidebar">
-    <button class="switcher__item-languages switcher__item btn btn-switcher" data-key="ru">
-        RU
-    </button>
-    <button class="switcher__item-languages switcher__item btn btn-switcher" data-key="eng">
-        ENG
-    </button>
-    </div>
-</div>
-`;
-
-liElementMenu.innerHTML = tplContent;
